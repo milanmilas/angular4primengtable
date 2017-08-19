ac1ccb3ea010c0d07ac269896b085487a9f81a3d
Milan Milas <milanmilas@gmail.com>
2017-08-19 20:00:14 +0100

    working table with filtering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Car } from './car';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+
+  cars: Car[];
+
+  constructor() {
+    var car = new Car();
+    car.brand = 'yugo';
+    car.vin = "http://www.google.com";
+    this.cars = [ car ];
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DataTableModule } from 'primeng/primeng';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DataTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
